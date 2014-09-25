f32a61ea5ab1e49aec8155fec0035a1d5c659c96
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-09-25 10:01:09 +0300

    Add Date behavior

---
+++ Date.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace nre\behaviors;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+use yii\helpers\Json;
+
+/**
+ * Class Date.
+ * Behavior for encoding and decoding model fields as JSON.
+ *
+ * @author    Serhiy Vinichuk <serhiyvinichuk@gmail.com>
+ * @copyright 2014 NullReferenceException
+ * @license   MIT
+ */
+class Date extends Behavior
+{
+    public $format = 'd.m.Y';
+    public $fields = [];
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'encode',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'encode',
+            ActiveRecord::EVENT_AFTER_FIND => 'decode',
+            ActiveRecord::EVENT_AFTER_INSERT => 'decode',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'decode',
+        ];
+    }
+
+    public function encode()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            if (!empty($model->$field) && is_string($model->$field)) {
+                $model->$field = strtotime($model->$field);
+            }
+        }
+    }
+
+    public function decode()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            $model->$field = empty($model->$field) ? null : date($this->format, $model->$field);
+        }
+    }
+
+}
---
+++ Json.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace nre\behaviors;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+use yii\helpers\Json;
+
+/**
+ * Class Json.
+ * Behavior for encoding and decoding model fields as JSON.
+ *
+ * @author    Serhiy Vinichuk <serhiyvinichuk@gmail.com>
+ * @copyright 2014 NullReferenceException
+ * @license   MIT
+ */
+class Json extends Behavior
+{
+    public $fields = [];
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'arrayToJson',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'arrayToJson',
+            ActiveRecord::EVENT_AFTER_FIND => 'jsonToArray',
+            ActiveRecord::EVENT_AFTER_INSERT => 'jsonToArray',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'jsonToArray',
+        ];
+    }
+
+    public function arrayToJson()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            if (isset($model->$field)) {
+                $model->$field = Json::encode($model->$field);
+            }
+        }
+    }
+
+    public function jsonToArray()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            $model->$field = empty($model->$field) ? [] : Json::decode($model->$field);
+        }
+    }
+} 
