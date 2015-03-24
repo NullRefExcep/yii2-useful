1bb571456871d6ae0490df37c1e79a5efaf54b16
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-03-24 11:41:02 +0200

    add password trait

---
+++ src/PasswordTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace nullref\useful;
+
+use Yii;
+
+/**
+ * Trait PasswordTrait
+ *
+ * @property $password
+ * @property $passwordHash
+ */
+trait PasswordTrait
+{
+    /**
+     * @var string $_password
+     */
+    protected $_password;
+
+    /**
+     * @var string
+     */
+    public $passwordConfirm;
+
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+    public function setPassword($value)
+    {
+        if (empty($value)) return;
+        $this->passwordHash = Yii::$app->security->generatePasswordHash($value);
+        $this->_password = $value;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param  string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->passwordHash);
+    }
+} 
