eb35a31bec91769176039c307b8bbef3ce04ee2c
Tenfrow <vit2zremb@gmail.com>
2016-01-27 16:40:23 +0200

    Add SerializeBehavior

---
+++ src/SerializeBehavior.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace nullref\useful;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+/**
+ * Class SerializeBehavior.
+ * Behavior for encoding and decoding model fields as storable representation of a value.
+ *
+ * @author    Tenfrow <vit2zremb@gmail.com>
+ * @copyright 2016 NullReferenceException
+ * @license   MIT
+ */
+class SerializeBehavior extends Behavior
+{
+    public $fields = [];
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'encode',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'encode',
+            ActiveRecord::EVENT_AFTER_FIND => 'decode',
+            ActiveRecord::EVENT_AFTER_INSERT => 'decode',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'decode',
+        ];
+    }
+
+    public function encode()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            if (isset($model->$field)) {
+                $model->$field = serialize($model->$field);
+            }
+        }
+    }
+
+    public function decode()
+    {
+        $model = $this->owner;
+        foreach ($this->fields as $field) {
+            $model->$field = unserialize($model->$field);
+        }
+    }
+} 
